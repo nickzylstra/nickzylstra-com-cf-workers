866a7deea4d5b260e2c0d435647d305ab655dc0e
nickzylstra <nick.zylstra@gmail.com>
2020-03-17 18:24:58 -0700

    Update Jest to handle CSS modules

--- jest.config.js
+++ jest.config.js
@@ -82,9 +82,9 @@ module.exports = {
   ],
 
   // A map from regular expressions to module names that allow to stub out resources with a single module
-  // moduleNameMapper: {
-  //   '\\.(css|less|scss|sss|styl)$': '<rootDir>/node_modules/jest-css-modules',
-  // },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sss|styl)$': '<rootDir>/node_modules/jest-css-modules',
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
