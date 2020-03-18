ec7d025ac5893cc1f834f6040b6aa18ab0f4fa9f
nickzylstra <nick.zylstra@gmail.com>
2020-03-17 21:55:16 -0700

    Position main content below navbar

--- src/client/components/Header.jsx
+++ src/client/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 import './Header.css';
 
@@ -7,13 +8,13 @@ const Header = ({ theme }) => {
   const { variant, bg } = theme;
   return (
     <Navbar fixed="top" collapseOnSelect expand="sm" variant={variant} bg={bg}>
-      <Navbar.Brand href="/">nz</Navbar.Brand>
+      <Navbar.Brand as={Link} to="/">nz</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav-header" />
       <Navbar.Collapse id="responsive-navbar-nav-header">
         <Nav defaultActiveKey="/">
-          <Nav.Link href="/">home</Nav.Link>
-          <Nav.Link href="#portfolio">portfolio</Nav.Link>
-          <Nav.Link href="#about">about</Nav.Link>
+          <Nav.Link as={Link} to="/">home</Nav.Link>
+          <Nav.Link as={Link} to="portfolio">portfolio</Nav.Link>
+          <Nav.Link as={Link} to="about">about</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
