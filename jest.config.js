54ccf811543c33ed860f7e17f188c0b460ce17c0
nickzylstra <nick.zylstra@gmail.com>
2020-03-19 12:42:32 -0700

    Update test thresholds

--- jest.config.js
+++ jest.config.js
@@ -47,10 +47,10 @@ module.exports = {
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
-      branches: 70,
-      functions: 70,
-      lines: 70,
-      statements: 70,
+      branches: 60,
+      functions: 80,
+      lines: 80,
+      statements: 80,
     },
   },
 
