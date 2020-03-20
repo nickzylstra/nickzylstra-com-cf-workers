e296ecc9fff090d27569138924bee09bb2b6413e
nickzylstra <nick.zylstra@gmail.com>
2020-03-19 18:31:39 -0700

    Address active selection on back bug with navbar

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, Suspense, lazy } from 'react';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Container, Spinner } from 'react-bootstrap';
 import Header from './components/Header.jsx';
@@ -16,27 +16,21 @@ const theme = {
 };
 
 // eslint-disable-next-line no-unused-vars
-const App = ({ host }) => {
-  const initTestState = 'init test state';
-  // eslint-disable-next-line no-unused-vars
-  const [testState, setTestState] = useState(initTestState);
-
-  return (
-    <div className="App" aria-label="app">
-      <Router>
-        <Header theme={theme} />
-        <Container as="main" bsPrefix="container-page">
-          <Suspense fallback={<Spinner animation="border" />}>
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route path="/portfolio" component={Portfolio} />
-              <Route path="/about" component={About} />
-            </Switch>
-          </Suspense>
-        </Container>
-      </Router>
-    </div>
-  );
-};
+const App = ({ host }) => (
+  <div className="App" aria-label="app">
+    <Router>
+      <Header theme={theme} />
+      <Container as="main" bsPrefix="container-page">
+        <Suspense fallback={<Spinner animation="border" />}>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/portfolio" component={Portfolio} />
+            <Route path="/about" component={About} />
+          </Switch>
+        </Suspense>
+      </Container>
+    </Router>
+  </div>
+);
 
 export default App;
--- src/client/components/Header.jsx
+++ src/client/components/Header.jsx
@@ -1,42 +1,49 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import './Header.css';
 
 const Header = ({ theme }) => {
   const { variant, bg } = theme;
-  const path = window.location.pathname.substring(1) || '/';
+  const { pathname } = useLocation();
+
   return (
     <Navbar fixed="top" collapseOnSelect expand="sm" variant={variant} bg={bg}>
       <Navbar.Brand href="/">nz</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav-header" />
       <Navbar.Collapse id="responsive-navbar-nav-header">
-        <Nav defaultActiveKey={path}>
-          <Nav.Link
-            as={Link}
-            to="/"
-            eventKey="/"
-            aria-label="home-link"
-          >
-            home
-          </Nav.Link>
-          <Nav.Link
-            as={Link}
-            to="portfolio"
-            eventKey="portfolio"
-            aria-label="portfolio-link"
-          >
-            portfolio
-          </Nav.Link>
-          <Nav.Link
-            as={Link}
-            to="about"
-            eventKey="about"
-            aria-label="about-link"
-          >
-            about
-          </Nav.Link>
+        <Nav activeKey={pathname}>
+          <Nav.Item>
+            <Nav.Link
+              as={Link}
+              to="/"
+              eventKey="/"
+              aria-label="home-link"
+            >
+              home
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link
+              as={Link}
+              to="/portfolio"
+              eventKey="/portfolio"
+              aria-label="portfolio-link"
+            >
+              portfolio
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link
+              as={Link}
+              to="/about"
+              eventKey="/about"
+              aria-label="about-link"
+            >
+              about
+            </Nav.Link>
+          </Nav.Item>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
