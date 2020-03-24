7ee3f55da981aa8ea8d5b5b472fc6f9ddcb35a3c
nickzylstra <nick.zylstra@gmail.com>
2020-03-23 18:14:03 -0700

    Add automatic linking of CSS bundles to index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,22 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const isDevelopment = process.env.NODE_ENV === 'development';
+
+const publicDirPath = path.resolve(__dirname, 'public');
 
 module.exports = {
   mode: isDevelopment ? 'development' : 'production',
   entry: path.resolve(__dirname, 'src', 'client', 'index.jsx'),
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),
+    path: publicDirPath,
     publicPath: '/',
   },
   devtool: isDevelopment ? 'inline-source-map' : false,
   devServer: {
-    contentBase: path.resolve(__dirname, 'public'),
+    contentBase: publicDirPath,
     port: 4000,
     watchContentBase: true,
     historyApiFallback: true,
@@ -67,6 +70,9 @@ module.exports = {
     ],
   },
   plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(publicDirPath, 'index.html'),
+    }),
     new MiniCssExtractPlugin({
       filename: isDevelopment ? '[name].css' : '[name].[hash].css',
       chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css',
