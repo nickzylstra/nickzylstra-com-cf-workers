f47945a8cf554ce21752351f2b26f7548b253f1f
nickzylstra <nick.zylstra@gmail.com>
2020-03-26 22:02:38 -0700

    Add margin to about page

--- src/client/components/About/About.jsx
+++ src/client/components/About/About.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 import Summary from './Summary';
 import Contact from './Contact/Contact';
 
 
 const About = () => (
-  <div aria-label="about-page">
+  <Container aria-label="about-page">
     <Summary />
     <Contact />
-  </div>
+  </Container>
 );
 
 export default About;
