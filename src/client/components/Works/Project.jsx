2bfffe7337e567e93049c485b4a100371dca047d
nickzylstra <nick.zylstra@gmail.com>
2020-03-28 16:31:17 -0700

    Style cards to use buttons

--- src/client/components/Works/Project.jsx
+++ src/client/components/Works/Project.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Card, Button } from 'react-bootstrap';
-import theme from '../../theme';
+import GithubIcon from '../About/Contact/GithubIcon';
 
 import styles from './Project.module.scss';
 
@@ -17,14 +17,14 @@ const Project = ({ project, handleModalShow }) => {
   const openModal = () => handleModalShow(id);
 
   return (
-    <Card id={`card-${id}`} className={`${styles.project} bg-${theme.bg} text-${theme.text} ${styles.hoverparent}`}>
+    <Card id={`card-${id}`} className={`${styles.project} ${styles.hoverparent}`}>
       <Header>
         <Title as="h6" onClick={openModal}>{name}</Title>
       </Header>
       <Img src={image} crossOrigin="anonymous" alt={`${name} project card image`} className={styles.imgHoverchild} />
       <ImgOverlay className={styles.contentHoverchild}>
-        <Button onClick={openModal}>Learn more!</Button>
-        <Card.Link href={githubLink} target="_blank" rel="noreferrer noopener">Github</Card.Link>
+        <Button variant="secondary" onClick={openModal}>Learn more!</Button>
+        <GithubIcon url={githubLink} />
       </ImgOverlay>
     </Card>
   );
