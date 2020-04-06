bce4ee5a8a3b38c3befb89209192d8c395225d05
nickzylstra <nick.zylstra@gmail.com>
2020-04-06 09:40:58 -0700

    Update about for CF current status

--- src/client/components/About/Summary.jsx
+++ src/client/components/About/Summary.jsx
@@ -30,7 +30,7 @@ const Summary = () => (
         {' '}
         <a href="https://www.mckinstry.com" target="_blank" rel="noopener noreferrer">McKinstry</a>
         , making facilities energy efficient.
-        Soon I start at
+        Now I&apos;m at
         {' '}
         <a href="https://www.cloudflare.com" target="_blank" rel="noopener noreferrer">Cloudflare</a>
         , building a better internet.
