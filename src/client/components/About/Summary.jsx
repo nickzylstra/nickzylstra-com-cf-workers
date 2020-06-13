e07ad11bfcff27eb483ead79170ccff58eeddb8e
nickzylstra <nick.zylstra@gmail.com>
2020-06-13 14:45:59 -0700

    Update summary

--- src/client/components/About/Summary.jsx
+++ src/client/components/About/Summary.jsx
@@ -42,7 +42,7 @@ const Summary = () => (
         Later on, coding turned into things like creating software tools streamlining the
         development of large construction projects, running Ethereum nodes,
         and customizing keyboard firmware.  Today I&apos;m building full stack web applications
-        with technologies like React, Node.js, PostgreSQL, Docker, AWS, and Cloudflare.
+        with technologies like React, Typescript, Node.js, PostgreSQL, Docker, Kubernetes, AWS, and Cloudflare.
       </p>
       <p>
         Why do I code?  Because it&apos;s really fun to me, plain and simple.
